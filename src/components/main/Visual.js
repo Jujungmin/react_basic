0612bb19303e75f5352b5e5059c03857b022e7aa
jungmin <43zhuzhu@gmail.com>
2022-05-01 13:00:54 +0900

    기본 swiper기능연결

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css';
 
 function Visual() {
-	return <figure className='myScroll'>Visual</figure>;
+	return (
+		<figure className='myScroll'>
+			<Swiper
+				loop={true}
+				slidesPerView={3}
+				spaceBetween={30}
+				pagination={{ clickable: true }}
+				navigation={true}
+				modules={[Pagination, Navigation]}
+				className='swiper'>
+				<SwiperSlide>1</SwiperSlide>
+				<SwiperSlide>2</SwiperSlide>
+				<SwiperSlide>3</SwiperSlide>
+				<SwiperSlide>4</SwiperSlide>
+				<SwiperSlide>5</SwiperSlide>
+			</Swiper>
+		</figure>
+	);
 }
 
 export default Visual;
